cf26db9070d63a22fbc8fdcc7cb0bc07ac0313b4
tangjun <1031500697@qq.com>
2017-05-31 16:46:14 +0800

    '百度地图'

--- map/demo.js
+++ map/demo.js
@@ -1,13 +1,69 @@
 // ================= demo
 // 加载地图
 function loadMap(){
+	// 实例化
 	var point = new BMap.Point(104.071216, 30.576279);
-	var map = new BaiduMap('mapContainer', point);
-
+	map = new BaiduMap('mapContainer', point);
+		
 	// 根据IP定位
-	// mapControl.locationByIp(function(result){
-	// 	var map = new BaiduMap('mapContainer', result.center);
-	// });
-	
+	map.localCityByIp(function(result){
+		map.bmap.setCenter(result.name);
+	});
+
+	// 获取地图显示范围
+	var bs = map.getBounds();
+	var sw = bs.getSouthWest();   		//可视区域左下角
+	var ne = bs.getNorthEast();   		//可视区域右上角
+	// console.log("当前地图可视范围是：" + sw.lng + "," + sw.lat + "到" + ne.lng + "," + ne.lat);
+
+
+	// 测距
+	var pointA = new BMap.Point(104, 30);
+	var pointB = new BMap.Point(105, 31);
+	var d = map.getDistance(pointA, pointB);
+	// console.log('两点距离是：'+ d + '米');
+
+
+// ==========================================================================================
+	var geolocation = new BMap.GeolocationControl();						//左下角，添加定位控件
+	var navigation = new BMap.NavigationControl();							//左上角，添加默认缩放平移控件
+	var cityList = new BMap.CityListControl({           					//左上角，添加城市列表控件
+				    anchor: BMAP_ANCHOR_TOP_LEFT, 
+				    offset: new BMap.Size(80, 15)          
+				})
+
+	window.demo = {
+		// 添加控件
+		addControls: function(){
+			map.addControls({
+				'cityList': cityList,
+				'navigation': navigation,        					
+				'geolocation': geolocation						
+			})
+		},
+
+		// 删除控件
+		removeControls: function(){
+			map.removeControls(['cityList'])
+		},
+
+		// 添加覆盖物
+		addOverlays: function(){
+			map.addOverlays({
+				'marker': new BMap.Marker(point)
+			});
+		},
+
+		// 删除覆盖物
+		removeOverlays: function(){
+			map.removeOverlays(['marker'])
+		},
+
+		// 删除所有覆盖物
+		clearOverlays: function(){
+			map.clearOverlays();
+		},
+	};
 }
+
 
--- map/map.js
+++ map/map.js
@@ -2,27 +2,165 @@
 
 /**
  * 构造器
+ */
+function BaiduMap(id, point){
+	if(!id) return;
+	this.bmap = undefined;          // map实例化对象
+	this.controls = {};              // 存放控件数组
+	this.overlays = {};              // 存放覆盖物数组
+	return this.init(id, point);
+}
+
+/**
+ * 初始化
  * 
  * id  选择器
  * point   位置坐标
  */
-function BaiduMap(id, point){
-	if(!id) return;
-	this.map = new BMap.Map(id, {enableMapClick:false});    
-	this.map.centerAndZoom(point, 12);
-	this.map.enableScrollWheelZoom(true); 
-}
-
-
-var mapControl = {
-	/**
-	 * 根据IP定位
-	 */
-	locationByIp: function(callback) {
-		var city = new BMap.LocalCity();
-		city.get(callback);
-	}
+BaiduMap.prototype.init = function(id, point){
+	this.bmap = new BMap.Map(id, {enableMapClick:false});    
+	this.bmap.centerAndZoom(point, 12);
+	this.bmap.enableScrollWheelZoom(true); 
+	return this;
 };
 
+// ===================  地图显示
+
+/**
+ * 获取地图显示范围
+ * 
+ */
+BaiduMap.prototype.getBounds = function() {
+	return this.bmap.getBounds();       
+};
+
+
+/**
+ * 测距
+ * 
+ */
+BaiduMap.prototype.getDistance = function(pointA, pointB) {
+	return this.bmap.getDistance(pointA, pointB).toFixed(2);
+};
+
+
+
+
+// ===================  控件
+
+/**
+ * 添加控件
+ * 
+ * controls {name: 别名，control: 对象}  控件对象
+ */
+BaiduMap.prototype.addControls = function(controls) {
+	var currKeys = Object.keys(this.controls);
+	var keys = Object.keys(controls);
+
+	for (var i = 0, len = keys.length; i < len; i++) {
+		var name = keys[i];
+		if(currKeys.indexOf(name) === -1) {
+			this.bmap.addControl(controls[keys[i]]);
+			this.controls[name] = controls[name];
+		}
+		
+	}
+	return this;
+};
+
+/**
+ * 删除控件
+ * 
+ * controls  控件名字的数组
+ */
+BaiduMap.prototype.removeControls = function(controls) {
+	var currKeys = Object.keys(this.controls);
+
+	for (var i = 0, len = controls.length; i < len; i++) {
+		var name = controls[i];
+		if(currKeys.indexOf(name) !== -1) {
+			this.bmap.removeControl(this.controls[name]);
+			delete this.controls[name];
+		}
+	}
+	return this;
+};
+
+
+// ===================  覆盖物
+
+/**
+ * 添加覆盖物
+ * 
+ * overlays {name: 别名，overlay: 对象}  覆盖物对象
+ */
+BaiduMap.prototype.addOverlays = function(overlays) {
+	var currKeys = Object.keys(this.overlays);
+	var keys = Object.keys(overlays);
+
+	for (var i = 0, len = keys.length; i < len; i++) {
+		var name = keys[i];
+		if(currKeys.indexOf(name) === -1) {
+			this.bmap.addOverlay(overlays[keys[i]]);
+			this.overlays[name] = overlays[name];
+		}
+		
+	}
+	return this;
+};
+
+/**
+ * 删除覆盖物
+ * 
+ * overlays  控件名字的数组
+ */
+BaiduMap.prototype.removeOverlays = function(overlays) {
+	var currKeys = Object.keys(this.overlays);
+
+	for (var i = 0, len = overlays.length; i < len; i++) {
+		var name = overlays[i];
+		if(currKeys.indexOf(name) !== -1) {
+			this.bmap.removeOverlay(this.overlays[name]);
+			delete this.overlays[name];
+		}
+	}
+	return this;
+};
+
+/**
+ * 删除所有覆盖物
+ * 
+ */
+BaiduMap.prototype.clearOverlays = function() {
+	this.bmap.clearOverlays(); 
+	this.overlays = {};
+	return this;
+};
+
+
+
+// ===================  定位
+/**
+ * 根据IP定位
+ * 
+ * callback  回调函数
+ */
+BaiduMap.prototype.localCityByIp = function(callback) {
+	var city = new BMap.LocalCity();
+	city.get(callback);
+	return this;
+};
+
+
+// ====================   Polyfill
+
+/**
+ * Object.keys
+ */
+if (!Object.keys) Object.keys = function(o) {
+  var k=[],p;
+  for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
+  return k;
+}
 
 
