4322530aecfcfe5c9c1acef321b6904cd0cc82ec
tangJun <1031500697@qq.com>
2017-06-04 22:19:45 +0800

    jq

--- jq/jq.custom.js
+++ jq/jq.custom.js
@@ -1,4 +1,6 @@
 (function(window, undefined) {
+	var slice = Array.prototype.slice;
+
 	var $ = function(selector, context) {
 		return new jQuery(selector, context);
 	};
@@ -13,8 +15,62 @@
 	};
 	
 	jQuery.fn = jQuery.prototype = {
-		
+		/**
+		  * 转化成真正的数组
+		  */
+		toArray: function(){
+			return slice.call(this, 0);
+		},
+		/**
+		  * 返回指定位置的元素
+		  */
+		get: function(num){
+			return num == null ? this.toArray() : (num < 0 ? this[this.length + num] : this[num]);
+		},
+		/**
+		  * 遍历迭代
+		  */
+		each: function(callback){
+			return $.each(this, callback);
+		},
+		/**
+		  * 返回一个新数组
+		  */
+		map: function(callback){
+			return $.map(this, callback);
+		},
+		/**
+		  * 筛选
+		  */
+		eq: function(i){
+			i = +i;
+			return i === -1 ? slice.call(this, i) : slice.call(this, i, i+1);
+		},
 	};
+ 
+	// =============================== 静态方法
+
+	/**
+	  * 遍历迭代
+	  */
+	$.each = function(array, callback){
+		for (var i = 0; i < array.length; i++) {
+			callback.call(array[i], i, array[i], array);
+		}
+		return array;
+	};
+
+	/**
+	  * 返回一个新数组
+	  */
+	$.map = function(array, callback){
+		var result = [];
+		for (var i = 0; i < array.length; i++) {
+			result.push(callback.call(array[i], i, array[i], array));
+		}
+		return result;
+	};
+
 
 	window.jQuery = window.$ = $;
 
