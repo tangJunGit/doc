03736825ef65059e3c58ced789d0133c96e85581
tangjun <1031500697@qq.com>
2017-06-09 17:27:51 +0800

    jq

--- jq/jq.custom.js
+++ jq/jq.custom.js
@@ -13,7 +13,8 @@
 			type = typeof selector;
 
 		if(selector[0] === '#'){                               	// $('#id')
-			nodes = [document.getElementById(selector.slice(1))];
+			nodes = document.getElementById(selector.slice(1));
+			nodes = nodes === null ? [] : [nodes];
 		}else if(type ===  'string' && context == null){        // css选择器
 			nodes = document.querySelectorAll(selector);
 		}else if(type ===  'string' && context.length > 0){     // 查找儿子节点                   
@@ -99,7 +100,7 @@
 		// 获取与添加属性
 		attr: function(name, value){
 			return value == null ? this[0].getAttribute(name)
-								: $.each(this, function(i, elem){elem.setAttribute(name, value+'')});
+								: $.each(this, function(i, elem){elem.setAttribute(name.tolowercase(), value+'')});
 		},
 		// 删除属性
 		removeAttr: function(name){
@@ -168,28 +169,98 @@
 					}
 				});
 		},
-		// 取value值
+		// 获取与赋值value值
 		val: function(value){
 			return value == null ? $.trim(this[0].value)
 								: $.each(this, function(i, elem){elem.value = value});
+		},
+		// html
+		html: function(html){
+			return html == null ? this[0].innerHTML
+								: $.each(this, function(i, elem){elem.innerHTML = html});
+		},
+		// 文本
+		text: function(text){
+			return text == null ? this[0].innerText
+								: $.each(this, function(i, elem){elem.innerText = text});
 		},
 
 
 		// ===============================  事件
 
-		// 绑定事件委托
+		// 绑定事件委托   selector 只包括 元素（如div），class名
 		on: function(types, selector, fn){
-			if(fn == null) fn = selector;
+			if(!fn){
+				fn = selector;
+				selector = undefined;
+			};
 			return $.each(this, function(i, elem) {
 				$.event.add( elem, types, fn, selector );
 			});
 		},
 		// 解除绑定事件
 		off: function(types, selector, fn){
-			if(fn == null) fn = selector;
+			if(!fn){
+				fn = selector;
+				selector = undefined;
+			};
 			return $.each(this, function(i, elem) {
 				$.event.remove( elem, types, fn, selector );
 			});
+		},
+
+		// ==================================  数据存储
+
+		// 获取与添加数据   
+		data: function(name, value, typeName){
+			var index, jqNameObj, typeNameObj,
+				jqName = 'jQuery';
+				typeName = typeName || 'data';
+
+			if(value === undefined){
+				index = this.hasData(name, typeName);
+				return  index !== -1? this[index][jqName][typeName][name] : undefined;
+			}
+
+			return $.map(this, function(i, elem){
+				jqNameObj = elem[jqName] || (elem[jqName] = {}),
+			 	typeNameObj = jqNameObj[typeName] || (elem[jqName][typeName] = {});
+					
+				elem[jqName][typeName][name] = value;
+				return elem;
+			});
+		},
+		// 删除存储数据
+		removeData: function(name, typeName){
+			var jqName = 'jQuery';
+				typeName = typeName || 'data';
+
+			return $.map(this, function(i, elem){
+				if($(elem).hasData(name, typeName) !== -1){
+					delete elem[jqName][typeName][name];
+				}
+				return elem;
+			});
+		},
+		// 检验是否存在数据
+		hasData: function(name, typeName){
+			var jqName = 'jQuery';
+			typeName = typeName || 'data';
+
+			for (var i = 0; i < this.length; i++) {
+				if(this[i][jqName] && this[i][jqName][typeName] &&  this[i][jqName][typeName][name]) return i;
+			}
+			return -1;
+		},
+
+		// ==================================  样式
+
+		// css
+		css: function(name, value){
+			value === undefined ?  return $.getStyle(elem).getPropertyValue(name) :
+				return $.each(this, function(i, elem){
+					
+				});
 		},
 
 	};
@@ -230,10 +301,12 @@
 
 	// 事件
 	$.event = {
+		// 绑定事件
 		add: function(elem, types, handler, selector){
 			types = types.split(" ");
 
 			$.each(types, function(i, type){
+
 				if ( elem.addEventListener ) {
 					elem.addEventListener( type, handler, false );
 				}else if( elem.attachEvent ) {
@@ -242,6 +315,7 @@
 			});
 			
 		},
+		// 解绑事件
 		remove: function(elem, types, handler, selector){
 			types = types.split(" ");
 
@@ -253,13 +327,26 @@
 				}
 			});
 			
-		}
+		},
 	};
+
+
+	// 获取css样式
+	$.getStyle = function(elem){
+		if ( elem.ownerDocument.defaultView.opener ) {
+			return elem.ownerDocument.defaultView.getComputedStyle( elem, null );
+		}
+
+		return window.getComputedStyle( elem, null );
+	};
+
 
 	window.jQuery = window.$ = $;
 
-	if ( typeof define === "function" && define.amd && define.amd.$ ) {
-		define( "jquery", [], function () { return $; } );
+	if ( typeof define === "function" && define.amd ) {
+		define( "jquery", [], function() {
+			return $;
+		});
 	}
     
 })(window);
